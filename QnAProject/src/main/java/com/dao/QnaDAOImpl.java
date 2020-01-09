7593028ae93b07d921f2c426cf2e8179fc51e375
lllilllilllilili <tqbs1024@gmail.com>
2020-01-09 18:10:08 +0900

    feat: 🎸 spring legacy + boot + vue axios connection

---
+++ QnAProject/src/main/java/com/dao/QnaDAOImpl.java
@@ -0,0 +1,61 @@
+package com.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.vo.Qna;
+
+@Component("qdao")
+public class QnaDAOImpl implements QnaDAO{
+
+	@Autowired
+	SqlSession session; //sqlSession 만들?��진거 ?��?�� ?��?��줘라. 
+	
+	public List<Qna> selectAll() {
+		// TODO Auto-generated method stub
+		return session.selectList("qna.selectAll");
+	}
+
+	public Qna selectOne(String num) {
+		// TODO Auto-generated method stub
+		return session.selectOne("qna.selectOne", num);
+	}
+
+	public void insert(Qna c) {
+		// TODO Auto-generated method stub
+		session.insert("qna.insert", c);
+	}
+
+	public void delete(String num) {
+		// TODO Auto-generated method stub
+		session.delete("qna.delete", num);
+	}
+	
+	public List<Qna> findByTitle(String title) {
+		// TODO Auto-generated method stub
+		return session.selectList("qna.findByTitle",title);
+	}
+
+	public List<Qna> findByName(String name) {
+		// TODO Auto-generated method stub
+		return session.selectList("qna.findByName",name);
+	}
+
+	public void update(String num,String ans) {
+		// TODO Auto-generated method stub
+		HashMap<String , String> map = new HashMap();
+		map.put("num", num);
+		map.put("ans",ans);
+		session.update("qna.update", map);
+		
+	}
+
+
+	
+	
+}
